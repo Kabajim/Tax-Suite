bae543de0283f3b55ec045e27e82cfb854d48867
DavidWieland88 <kabajim@gmx.de>
2018-09-03 10:46:30 +0200

    Remove dummy data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
-import { addLinkContainer } from './actions/dashboard'
+
 
 const store = configureStore();
-
-store.dispatch(addLinkContainer({name: "TestContainer1"}));
-store.dispatch(addLinkContainer({name: "TestContainer2"}));
 
 const jsx = (
     <Provider store={store}>
